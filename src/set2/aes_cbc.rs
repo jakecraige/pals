bd93173f6003ed5fe9bb21d22b083b14d68ecda8
Jake Craige <james.craige@gmail.com>
2018-10-12 08:34:58 -0700

    Set 2: Challenge 10, implement CBC done
    Ended up being pretty gnarly to use the openssl lib and have it _not_
    pad by default since I was implementing it. Due to this, I also needed
    to implement detecting and removing the padding on the output.
    
    Turns out that is Challenge 15 so that's kind of done too, but I'll add
    a test for their examples when I get there.

--- src/set2/aes_cbc.rs
+++ src/set2/aes_cbc.rs
@@ -53,18 +53,52 @@ fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, ErrorStack> {
     Ok(ciphertext)
 }
 
-fn decrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, ErrorStack> {
+fn decrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, PaddingError> {
     let mut offset_data = iv.to_vec();
     offset_data.append(&mut data.to_vec());
 
-    let plaintext = data.chunks(BYTES_IN_BLOCK)
+    let plaintext: Vec<u8> = data.chunks(BYTES_IN_BLOCK)
         .zip(offset_data.chunks(BYTES_IN_BLOCK))
         .map(|(block, prev_block)| {
+            // This is unsafe to unwrap. In a real impl we'd want to handle and return an err.
             let decrypted_block = openssl_ecb_decrypt_block(&block, &key).unwrap();
             xor_slices(&decrypted_block, &prev_block)
         }).flatten().collect();
 
-    Ok(plaintext)
+    remove_padding(&plaintext)
+}
+
+#[derive(Debug, PartialEq)]
+struct PaddingError(u8, u8); // expected, actual
+
+fn remove_padding(data: &[u8]) -> Result<Vec<u8>, PaddingError> {
+    if data.len() == 0 {
+        return Ok(data.to_vec());
+    }
+
+    let mut mdata = data.to_vec();
+    let mut ctr = 1u8;
+    let padding = mdata.pop().unwrap();
+
+    // Walk backwards from the end and built up the counter of how many bytes of padding we find
+    // that match what's expected.
+    while let Some(byte) = mdata.pop() {
+        // Increment counter iff it's the same, and we haven't already met the count needed
+        if byte == padding && ctr != padding {
+            ctr += 1;
+        } else {
+            // Since we pop it off in the let, but it was not padding, we need to add it back in
+            // here.
+            mdata.push(byte);
+            break;
+        }
+    }
+
+    if ctr == padding {
+        Ok(mdata)
+    } else {
+        Err(PaddingError(padding, ctr))
+    }
 }
 
 #[cfg(test)]
@@ -72,41 +106,52 @@ mod tests {
     use set2::aes_cbc;
     use std::fs::File;
     use std::io::Read;
+    use base64::decode as base64decode;
 
     #[test]
-    fn aes_cbc_encrypt() {
+    fn aes_cbc_encrypt_and_decrypt() {
         let data = b"Hellooooo";
         let key = b"YELLOW SUBMARINE";
         let iv = b"YELLOW SUBMARINE";
 
         let ciphertext = aes_cbc::encrypt(data, key, iv).unwrap();
-
         assert_eq!(
             ciphertext,
             b"\x3d\x88\xc2\x16\x5f\xf7\xa8\x09\x24\xe3\xb7\xae\x66\x90\x90\xa9"
         );
 
         let plaintext = aes_cbc::decrypt(&ciphertext, key, iv).unwrap();
+        assert_eq!(plaintext, b"Hellooooo");
+    }
 
-        // TOOD: Strip padding in decrypt
-        assert_eq!(plaintext, b"Hellooooo\x07\x07\x07\x07\x07\x07\x07");
-        assert!(false);
+    #[test]
+    fn remove_padding() {
+        // Valid padding of 7
+        let plaintext = b"Hello\x07\x07\x07\x07\x07\x07\x07";
+        let plaintext_no_pad = aes_cbc::remove_padding(plaintext).unwrap();
+        assert_eq!(plaintext_no_pad, b"Hello");
+
+        // Padding is 2, but there's 3 bytes of it. Should only remove two.
+        let plaintext = b"Hello\x02\x02\x02";
+        let plaintext_no_pad = aes_cbc::remove_padding(plaintext).unwrap();
+        assert_eq!(plaintext_no_pad, b"Hello\x02");
+
+        // Not enough padding
+        let plaintext = b"Hello\x07\x07\x07\x07";
+        let err = aes_cbc::remove_padding(plaintext).expect_err("expect padding error");
+        assert_eq!(err, aes_cbc::PaddingError(7, 4));
     }
 
     #[test]
     fn aes_cbc_decrypt_example() {
         let file = read_file("src/data/challenge10.txt", true);
-        let ciphertext = file.as_bytes();
+        let ciphertext = base64decode(&file).expect("should be decodable");
         let key = b"YELLOW SUBMARINE";
         let iv = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
-        println!("IV: {:?}", iv.len());
-
 
         let plaintext_bytes = aes_cbc::decrypt(&ciphertext, key, iv).unwrap();
-        let plaintext = String::from_utf8(plaintext_bytes).unwrap();
-        println!("Plaintext: {:?}", plaintext);
-        assert!(false);
-        // assert_eq!(plaintext, b"Hellooooo\x07\x07\x07\x07\x07\x07\x07");
+        let plaintext = String::from_utf8(plaintext_bytes).expect("valid utf8 text");
+        assert!(plaintext.contains("Lay down and boogie and play that funky music"));
     }
 
     // Helper to read a file from disk unsafely and strip newlines
