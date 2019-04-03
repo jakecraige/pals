44f0fefc1fa237209b493a4218aead60951c8782
Jake Craige <james.craige+git@gmail.com>
2019-04-03 10:21:50 -0700

    Beginning of point-wise arithmetic operations
    Start of refactor to implementing arithmetic operations on the points
    themselves to provide an easier to understand API. Eventually the curve
    will move into the point itself but passing it in as an option eases the
    transition during the refactor.

--- src/elliptic_curve.rs
+++ src/elliptic_curve.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use num_traits::*;
 use num_bigint::{BigInt};
 use finite_field::{Field, FieldElement};
@@ -19,6 +20,68 @@ impl Point {
             Point::Coordinate { x, y } => Point::Coordinate { x, y: -y },
         }
     }
+
+    pub fn add(&self, q: &Point, curve: &FiniteCurve) -> Point {
+        let p = self;
+        if p == &q.inverse() {
+            return Point::Infinity;
+        }
+
+        match (p, q) {
+            (Point::Infinity, _) => q.clone(),
+            (_, Point::Infinity) => p.clone(),
+
+            (Point::Coordinate {x: x_p, y: y_p}, Point::Coordinate {x: x_q, y: y_q}) => {
+                // We now have two non-zero, non-symmetric points to work with
+                let m = if x_p == x_q && y_p == y_p {
+                    let x_p2 = x_p * x_p;
+                    // Slope calculation is different when points are equal
+                    (x_p2 * BigInt::from(3) + &curve.a) / (y_p * BigInt::from(2))
+                } else {
+                    (y_p - y_q) / (x_p - x_q)
+                };
+
+                // Intersection of points
+                let m2 = &m * &m;
+                let x_r = m2 - x_p - x_q;
+                let y_r = y_q + (m * (&x_r - x_q));
+
+                // (x_p, y_p) + (x_q, y_q) = (x_r, -y_r)
+                Point::Coordinate { x: x_r, y: -y_r }
+            }
+        }
+    }
+
+    // Multiplication implemented using the double-and-add algorithm.
+    //
+    // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication#Double-and-add
+    pub fn mul<T: Into<BigInt> + Clone>(&self, n: &T, curve: &FiniteCurve) -> Point {
+        let mut coeff = n.clone().into();
+        let mut current = self.clone();
+        let mut result = Point::Infinity;
+
+        if coeff < BigInt::zero() {
+            panic!("Unexpected multiply by negative number");
+        }
+
+        while coeff > BigInt::zero() {
+            if !(&coeff & BigInt::one()).is_zero() {
+                result = result.add(&current, curve); // add
+            }
+            current = current.add(&current, curve); // double
+            coeff >>= 1;
+        }
+        result
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Point::Infinity => write!(f, "(inf, inf)"),
+            Point::Coordinate { x, y } => write!(f, "({}, {})", x, y)
+        }
+    }
 }
 
 // Elliptic Curve in Weierstrass normal form: y^2 = x^3 + ax + b
@@ -31,17 +94,17 @@ pub struct FiniteCurve {
 }
 
 impl FiniteCurve {
-    pub fn new(a: BigInt, b: BigInt, p: BigInt) -> Self {
+    pub fn new<T: Into<BigInt>>(a: T, b: T, p: T) -> Self {
         let field = Field::new(p);
         FiniteCurve { a: field.elem(a), b: field.elem(b), field }
     }
 
-    pub fn field_elem(&self, n: BigInt) -> FieldElement {
+    pub fn field_elem<T: Into<BigInt>>(&self, n: T) -> FieldElement {
         self.field.elem(n)
     }
 
-    pub fn point(&self, x: BigInt, y: BigInt) -> Point {
-        let (x, y) = (self.field_elem(x), self.field_elem(y));
+    pub fn point<T: Into<BigInt>>(&self, x: T, y: T) -> Point {
+        let (x, y) = (self.field_elem(x.into()), self.field_elem(y.into()));
         // TODO: Verify point on curve
         Point::coord(x, y)
     }
@@ -96,5 +159,31 @@ impl FiniteCurve {
             coeff >>= 1;
         }
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use elliptic_curve::*;
+
+    #[test]
+    fn elliptic_curve_point_add() {
+        let c = &FiniteCurve::new(-7, 10, 999999);
+        assert_eq!(c.point(1, 2).add(&c.point(1, -2), c), Point::Infinity); // add to inverse
+        assert_eq!(c.point(1, 2).add(&Point::Infinity, c), c.point(1, 2)); // add to infinity
+        assert_eq!(Point::Infinity.add(&c.point(1, 2), c), c.point(1, 2)); // add to infinity
+        assert_eq!(c.point(1, 2).add(&c.point(3, 4), c), c.point(-3, 2)); // add to another
+        assert_eq!(c.point(-1, 4).add(&c.point(1, 2), c), c.point(1, -2)); // add to another
+        assert_eq!(c.point(1, 2).add(&c.point(1, 2), c), c.point(-1, -4)); // add to self
+    }
+
+    #[test]
+    fn elliptic_curve_point_mul() {
+        let c = &FiniteCurve::new(2, 3, 97);
+
+        let res = c.point(3, 6).mul(&2, c);
+        let exp = c.point(80, 10);
+        println!("res: {}, exp: {}, add: {}", res, exp, c.point(3, 6).add(&c.point(3, 6), c));
+        assert_eq!(res, exp);
     }
 }
